61754274c49e56d0f116a4df3d498a47fdcabd6d
jcarloshg <carlosj12336@gmail.com>
2024-08-07 18:05:14 -0600

    import project

---
+++ src/main/java/com/mvc/validations/validationsdemo/ValidationsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.validations.validationsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationsdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvc/validations/validationsdemo/ValidationsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.validations.validationsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
